6f9f7b2f577549fe69365e18c9342b2d0a406d29
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-05-01 23:57:19 +0300

    eid

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropdownMenu = document.querySelector('.dropdown_menu')
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropdownMenu = document.querySelector(".dropdown_menu");
 
-toggleBtn.onclick = function(params)
+toggleBtn.onclick = function () {
+  dropdownMenu.classList.toggle("open");
+  const isOpen = dropdownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
