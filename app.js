6aa1b7a9ab90be4de724003004b24e99f14f34be
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-05-01 23:51:13 +0300

    eid

---
+++ app.js
@@ -0,0 +1,5 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropdownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function(params)
